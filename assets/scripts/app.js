c69518391f88c90186b9286ad69a6005933ccb93
Alexandre Mucarzel Fonseca <amfonseca98@gmail.com>
2023-02-02 17:25:06 -0300

    Finished Episode 23

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,34 +1,7 @@
-const backdropElement = document.getElementById('backdrop');
-const modalLinkElements = document.querySelectorAll('.info-modal');
-let infoModal;
+let currentResult = 0;
 
-function toggleBackdrop() {
-  backdropElement.classList.toggle('visible');
-}
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-function presentInfoModal(event) {
-  const text = event.target.dataset.text;
-  toggleBackdrop();
-  infoModal = document.createElement('div');
-  infoModal.classList.add('modal');
-  infoModal.innerHTML = `
-    <h2>More Details</h2>
-    <p>${text}</p>
-  `;
-  const closeButton = document.createElement('button');
-  closeButton.addEventListener('click', hideInfoModal);
-  closeButton.textContent = 'Okay';
-  infoModal.appendChild(closeButton);
-  document.body.appendChild(infoModal);
-}
+let calculationDescription = '((0 + 10) * 3) / 2 - 1';
 
-function hideInfoModal() {
-  toggleBackdrop();
-  document.body.removeChild(infoModal);
-}
-
-for (const linkElement of modalLinkElements) {
-  linkElement.addEventListener('click', presentInfoModal);
-}
-
-backdropElement.addEventListener('click', hideInfoModal);
+outputResult(currentResult, calculationDescription);
